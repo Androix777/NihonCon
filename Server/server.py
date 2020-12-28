d0c71ddd89087bb1605da02828ddd9ca58d4dab2
Androix <androix77@gmail.com>
2020-12-28 20:02:38 +0500

    Server json

--- Server/server.py
+++ Server/server.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from flask_cors import CORS
 from flask import request
+import spacy
+from jamdict import Jamdict
 
 app = Flask(__name__)
 CORS(app)
-
-values = []
+jmd = Jamdict()
+nlp = spacy.load("ja_core_news_lg")
 
 @app.route('/')
 def hello_world():
@@ -15,18 +18,17 @@ def hello_world():
 @app.route('/text', methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        if(len(values) > 0):
-            return(values.join(' '))
-        else:
-            return('')
+        return('No')
     
     if request.method == 'POST':
-        if(request.form.get('value') not in values):
-            values.append(request.form.get('value'))
-        if(len(values) > 0):
-            return(values[-1])
-        else:
-            return('')
+        allWords = []
+        
+        value = request.form.get('value')
+        doc = [(w.text, w.pos_) for w in (nlp(value))]
+        for word in doc:
+            allWords.append(word)
+        return Flask.jsonify(allWords)
+        
 
 if __name__ == '__main__':
     app.run()
