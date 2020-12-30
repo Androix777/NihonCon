07488f294257622529d0ac3a7caf1dd7c52eb639
Androix <androix77@gmail.com>
2020-12-30 20:16:46 +0500

    New server output

--- Server/server.py
+++ Server/server.py
@@ -11,31 +11,33 @@ CORS(app)
 jmd = Jamdict()
 nlp = spacy.load("ja_core_news_lg")
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/text', methods = ['GET', 'POST'])
+
+@app.route('/text', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return('No')
-    
+        return 'No'
+
     if request.method == 'POST':
         allWords = []
-        
+
         value = request.form.get('value')
         doc = [{'word': w.text} for w in (nlp(value))]
         jmd = Jamdict()
         for i in range(len(doc)):
             translate = jmd.lookup(doc[i]['word'])
-            if (len(translate.entries) > 0):
-                doc[i]['toolTip'] = str(translate.entries[0].senses[0].gloss)
-        
-        
+            if len(translate.entries) > 0:
+                doc[i]['toolTip'] = []
+                for entry in translate.entries:
+                    doc[i]['toolTip'].append({'description': [str(x.gloss) for x in entry], 'kana':str(entry.kana_forms)})
         for word in doc:
             allWords.append(word)
-        return jsonify({'ttText' : allWords})
-        
+        return jsonify({'ttText': allWords})
+
 
 if __name__ == '__main__':
     app.run()
