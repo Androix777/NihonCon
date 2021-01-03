0735e7dfc54031ec0051f67fd49c10ea2196dbac
Androix <androix77@gmail.com>
2021-01-03 05:04:56 +0500

    Fix (I hope)

--- Server/server.py
+++ Server/server.py
@@ -1,16 +1,35 @@
 from flask import Flask
 from flask import jsonify
-from flask import render_template
 from flask_cors import CORS
 from flask import request
 import spacy
 from jamdict import Jamdict
 
+from multiprocessing import Lock
+from multiprocessing.managers import BaseManager
+
 app = Flask(__name__)
 CORS(app)
-jmd = Jamdict()
+lock = Lock()
 nlp = spacy.load("ja_core_news_lg")
 
+def Tokenizer(line):
+    with lock:
+        nlp2 = nlp(line)
+    return [{'word': w.text} for w in nlp2]
+
+def GetTranslation(tokens):
+    jmd = Jamdict()
+    allWords = []
+    for i in range(len(tokens)):
+        translate = jmd.lookup(tokens[i]['word'])
+        if len(translate.entries) > 0:
+            tokens[i]['toolTip'] = []
+            for entry in translate.entries:
+                tokens[i]['toolTip'].append({'description': [str(x.gloss) for x in entry], 'kana':str(entry.kana_forms)})
+    for word in tokens:
+        allWords.append(word)
+    return jsonify({'ttText': allWords})
 
 @app.route('/')
 def hello_world():
@@ -23,20 +42,9 @@ def index():
         return 'No'
 
     if request.method == 'POST':
-        allWords = []
-
         value = request.form.get('value')
-        doc = [{'word': w.text} for w in (nlp(value))]
-        jmd = Jamdict()
-        for i in range(len(doc)):
-            translate = jmd.lookup(doc[i]['word'])
-            if len(translate.entries) > 0:
-                doc[i]['toolTip'] = []
-                for entry in translate.entries:
-                    doc[i]['toolTip'].append({'description': [str(x.gloss) for x in entry], 'kana':str(entry.kana_forms)})
-        for word in doc:
-            allWords.append(word)
-        return jsonify({'ttText': allWords})
+        tokens = Tokenizer(value)
+        return GetTranslation(tokens)
 
 
 if __name__ == '__main__':
