a600a3deb8b1a8a6ffc8fe9a644b00ffb03d5a46
Airplane <letadllo@gmail.com>
2021-01-29 18:43:37 +0500

    Edit profile

--- NHC/app/routes.py
+++ NHC/app/routes.py
@@ -1,10 +1,11 @@
 from app import app, db
 from app import Tokenizer, GetTranslation
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from flask import request, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 #UI
 
@@ -51,6 +52,21 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/edit_profile', methods = ['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Profile info changed')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title = 'Edit Profile', form = form)
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -86,3 +102,10 @@ def tooltip():
         tokens = Tokenizer(value)
         return GetTranslation(tokens)
     
+#misc
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
