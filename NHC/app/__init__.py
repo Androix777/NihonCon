ab5ad03844dbc9fccb84dbafccac93ec076afca0
Airplane <letadllo@gmail.com>
2021-01-30 16:03:16 +0500

    Logging

--- NHC/app/__init__.py
+++ NHC/app/__init__.py
@@ -32,7 +32,6 @@ def GetTranslation(tokens):
         allWords.append(word)
     return jsonify({'ttText': allWords})
 
-
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -44,3 +43,34 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+
+#logging
+
+import logging, os
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs = app.config['ADMINS'], subject = 'Error log',
+            credentials = auth, secure = secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes = 10240, backupCount = 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Logging started...')
