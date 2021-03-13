f6bfafcc3b0675ae679e650c3c344e0fefaa024f
Androix777 <androix77@gmail.com>
2021-03-13 20:58:11 +0500

    Re: ZERO

--- Queries/Kanji.sql
+++ Queries/Kanji.sql
@@ -1,4 +1,7 @@
 select kanji, count(kanji)
 from japanese_example_kanji
+left join japanese_example
+on japanese_example_kanji.japanese_example_row_id = japanese_example.row_id
+where origin > 0
 group by kanji
 order by count(kanji) desc
